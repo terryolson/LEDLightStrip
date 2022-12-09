1715d785e0f8ee139db620748a1bad7a927aa16c
terryolson <49874102+terryolson@users.noreply.github.com>
2022-12-09 06:10:27 -0500

    Create Christmas.py

---
+++ Christmas.py
@@ -0,0 +1,96 @@
+import plasma
+from plasma import plasma2040
+from pimoroni import Button
+import time
+
+#Define what an RGB Value is to add to our array later
+class RGBValue():
+    def __init__(self, r_val, g_val, b_val):
+        self.r=r_val
+        self.g=g_val
+        self.b=b_val
+
+# Define the number of LED's to work with
+NUM_LEDS = 110
+
+BRIGHTNESS = 1
+#Initalize the LED Strip
+
+led_strip = plasma.WS2812(NUM_LEDS, 0, 0, plasma2040.DAT)
+led_strip.start()
+
+# Define what our buttons are
+button_a = Button(plasma2040.BUTTON_A)
+button_b = Button(plasma2040.BUTTON_B)
+
+# Create a blank RGB Array.  This will be repeated over the lenght of the LED Strip
+rgbArray = []
+
+# Setup some default colors
+color_black  = RGBValue(0, 0, 0)
+color_white  = RGBValue(255,255,255)
+color_red    = RGBValue(255, 0, 0)
+color_green  = RGBValue(0, 255, 0)
+color_blue   = RGBValue(0, 0, 255)
+color_purple = RGBValue(128, 0, 128)
+color_cyan   = RGBValue(0, 255, 255)
+
+# Add values to the RGB Array
+rgbArray.append(color_red)
+rgbArray.append(color_red)
+rgbArray.append(color_red)
+rgbArray.append(color_black)
+rgbArray.append(color_black)
+rgbArray.append(color_black)
+rgbArray.append(color_green)
+rgbArray.append(color_green)
+rgbArray.append(color_green)
+rgbArray.append(color_black)
+rgbArray.append(color_black)
+rgbArray.append(color_black)
+
+# Clean up the LED Strip to start with
+for i in range(NUM_LEDS):
+    led_strip.set_rgb(i, 0, 0, 0)
+
+# Set some inital values
+current_array_pos = 0
+start_array_pos = 0
+pos_modifier = 1
+
+# Loop forever
+while True:
+    current_array_pos = start_array_pos
+    for i in range(NUM_LEDS):
+        # Write values to the LED Strip.  Loops through the Array of LED Values entered Above
+        led_strip.set_rgb(i,
+            rgbArray[current_array_pos].r,
+            rgbArray[current_array_pos].g,
+            rgbArray[current_array_pos].b)
+        current_array_pos = current_array_pos + 1
+        
+        # time.sleep(0.1)
+        # Keep our values in a valid range
+        if(current_array_pos >= len(rgbArray)):
+            current_array_pos = 0
+        if(current_array_pos < 0):
+            current_array_pos = len(rgbArray) - 1
+    
+    # Let user change the direction of the scrolling LED's with buttons
+    if(button_a.read()):
+        pos_modifier = 1
+        # print("Pressed A!")
+    if(button_b.read()):
+       pos_modifier = -1
+       # print("Pressed B!")
+    
+    start_array_pos = start_array_pos + pos_modifier
+    # Keep our values in a valid range
+    if(start_array_pos >= len(rgbArray)):
+        start_array_pos = 0
+    if(start_array_pos < 0):
+        start_array_pos = len(rgbArray) - 1
+
+    # Delay to determine the speed of the scrolling LED's
+    time.sleep(0.20)
+
